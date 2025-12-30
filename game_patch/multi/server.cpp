9c9feca9f6d7a1066aaefa6ca71579da3b398b8b
Chris Parsons <gooberCP@gmail.com>
2025-12-30 12:05:04 -0330

    block spawn requests from browser clients

--- game_patch/multi/server.cpp
+++ game_patch/multi/server.cpp
@@ -2011,6 +2011,9 @@ FunHook<void(rf::Player*)> multi_spawn_player_server_side_hook{
         if (g_alpine_server_config_active_rules.force_character.enabled) {
             player->settings.multi_character = g_alpine_server_config_active_rules.force_character.character_index;
         }
+        if (pdata.is_browser()) {
+            return;
+        }
         if (!check_can_player_spawn(player)) {
             return;
         }
